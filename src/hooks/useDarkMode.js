0d9b6bdd4e0d800a8ed2d0fad69f1aabae11049b
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-30 14:03:21 -0700

    create screenChange function.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,7 +3,7 @@ import { useLocalStorage} from './useLocalStorage';
 export const useDarkMode = (key, initialValues, cb) => {
     const [values, setValues] = useLocalStorage(key, initialValues)
 
-    const handleChanges = e => {
+    const screenChange = e => {
         console.log(e.target.name);
         setValues({
           ...values,
